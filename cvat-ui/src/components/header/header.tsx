278428f5f607b3f5482a6cf715aacfd643058182
acecode <acecode.tk@gmail.com>
2025-03-11 03:19:44 +0800

    feat: translate header.tsx

--- cvat-ui/src/components/header/header.tsx
+++ cvat-ui/src/components/header/header.tsx
@@ -28,6 +28,7 @@ import Button from 'antd/lib/button';
 import Dropdown from 'antd/lib/dropdown';
 import Modal from 'antd/lib/modal';
 import Text from 'antd/lib/typography/Text';
+import { useTranslation } from 'react-i18next';
 
 import config from 'config';
 
@@ -183,11 +184,38 @@ function HeaderComponent(props: Props): JSX.Element {
     } = config;
 
     const isMounted = useIsMounted();
+    const [listFetching, setListFetching] = useState(false);
+    const [organizationsList, setOrganizationList] = useState<Organization[] | null>(null);
+    const { t } = useTranslation();
+    const { t: tHeader } = useTranslation('header');
+
+    const searchCallback = useCallback((search?: string): Promise<Organization[]> => new Promise((resolve, reject) => {
+        const promise = core.organizations.get(search ? { search } : {});
+
+        setListFetching(true);
+        promise.then((organizations: Organization[]) => {
+            resolve(organizations);
+        }).catch((error: unknown) => {
+            reject(error);
+        }).finally(() => {
+            if (isMounted()) {
+                setListFetching(false);
+            }
+        });
+    }), []);
 
     useEffect(() => {
-        if (isMounted()) {
-            fetchOrganizations();
-        }
+        searchCallback().then((organizations: Organization[]) => {
+            if (isMounted()) {
+                setOrganizationList(organizations);
+            }
+        }).catch((error: unknown) => {
+            setOrganizationList([]);
+            notification.error({
+                message: tHeader('Could not receive a list of organizations'),
+                description: error instanceof Error ? error.message : '',
+            });
+        });
     }, []);
 
     const history = useHistory();
@@ -213,21 +241,21 @@ function HeaderComponent(props: Props): JSX.Element {
     aboutLinks.push([(
         <Col key='changelog'>
             <a href={CHANGELOG_URL} target='_blank' rel='noopener noreferrer'>
-                What&apos;s new?
+                {tHeader('about.What\'s new')}
             </a>
         </Col>
     ), 0]);
     aboutLinks.push([(
         <Col key='license'>
             <a href={LICENSE_URL} target='_blank' rel='noopener noreferrer'>
-                MIT License
+                {tHeader('about.MIT License')}
             </a>
         </Col>
     ), 10]);
     aboutLinks.push([(
         <Col key='discord'>
             <a href={DISCORD_URL} target='_blank' rel='noopener noreferrer'>
-                Find us on Discord
+                {tHeader('about.Find us on Discord')}
             </a>
         </Col>
     ), 20]);
@@ -243,11 +271,15 @@ function HeaderComponent(props: Props): JSX.Element {
                 <div>
                     <p>{`${about.server.description}`}</p>
                     <p>
-                        <Text strong>Server version:</Text>
+                        <Text strong>
+                            {`${tHeader('about.Server version')}:`}
+                        </Text>
                         <Text type='secondary'>{` ${about.server.version}`}</Text>
                     </p>
                     <p>
-                        <Text strong>UI version:</Text>
+                        <Text strong>
+                            {`${tHeader('about.UI version')}:`}
+                        </Text>
                         <Text type='secondary'>{` ${about.packageVersion.ui}`}</Text>
                     </p>
                     <Row justify='space-around'>
@@ -262,6 +294,7 @@ function HeaderComponent(props: Props): JSX.Element {
                     width: '100px',
                 },
             },
+            okText: t('OK'),
         });
     }, [about]);
 
@@ -302,7 +335,7 @@ function HeaderComponent(props: Props): JSX.Element {
             onClick: (): void => {
                 window.open('/admin', '_blank');
             },
-            label: 'Admin page',
+            label: tHeader('Admin page'),
         }, 0]);
     }
 
@@ -319,41 +352,54 @@ function HeaderComponent(props: Props): JSX.Element {
 
     menuItems.push([{
         key: 'organization',
-        icon: organizationFetching || organizationsListFetching ? <LoadingOutlined /> : <TeamOutlined />,
-        label: 'Organization',
-        disabled: organizationFetching || organizationsListFetching,
+        icon: organizationFetching || listFetching ? <LoadingOutlined /> : <TeamOutlined />,
+        label: t('Organization'),
+        disabled: organizationFetching || listFetching,
         children: [
             ...(currentOrganization ? [{
                 key: 'open_organization',
                 icon: <SettingOutlined />,
-                label: 'Settings',
+                label: t('Settings'),
                 className: 'cvat-header-menu-open-organization',
                 onClick: () => history.push('/organization'),
             }] : []), {
                 key: 'invitations',
                 icon: <MailOutlined />,
-                label: 'Invitations',
+                label: t('Invitations'),
                 className: 'cvat-header-menu-organization-invitations-item',
                 onClick: () => history.push('/invitations'),
             }, {
                 key: 'create_organization',
                 icon: <PlusOutlined />,
-                label: 'Create',
+                label: t('Create'),
                 className: 'cvat-header-menu-create-organization',
                 onClick: () => history.push('/organizations/create'),
             },
             ...(!!organizationsList && viewType === 'list' ? [{
                 key: 'switch_organization',
-                label: 'Switch organization',
+                label: tHeader('Switch organization'),
                 onClick: () => {
-                    openSelectOrganizationModal(setNewOrganization);
+                    Modal.confirm({
+                        title: tHeader('Select an organization'),
+                        okButtonProps: {
+                            style: { display: 'none' },
+                        },
+                        content: (
+                            <OrganizationsSearch
+                                defaultOrganizationList={organizationsList}
+                                resetOrganization={resetOrganization}
+                                searchOrganizations={searchCallback}
+                                setNewOrganization={setNewOrganization}
+                            />
+                        ),
+                    });
                 },
             }] : []),
             ...(!!organizationsList && viewType === 'menu' ? [{
                 type: 'divider' as const,
             }, {
                 key: '$personal',
-                label: 'Personal workspace',
+                label: tHeader('Personal workspace'),
                 className: !currentOrganization ? 'cvat-header-menu-active-organization-item' : 'cvat-header-menu-organization-item',
                 onClick: resetOrganization,
             }, ...organizationsList.map((organization: Organization) => ({
@@ -370,21 +416,32 @@ function HeaderComponent(props: Props): JSX.Element {
         icon: <SettingOutlined />,
         onClick: () => switchSettingsModalVisible(true),
         title: `Press ${switchSettingsShortcut} to switch`,
-        label: 'Settings',
-    }, 30]);
+        label: t('Settings'),
+    }, 20]);
 
     menuItems.push([{
         key: 'about',
         icon: <InfoCircleOutlined />,
         onClick: () => showAboutModal(),
-        label: 'About',
-    }, 40]);
+        label: t('About'),
+    }, 30]);
+
+    if (renderChangePasswordItem) {
+        menuItems.push([{
+            key: 'change_password',
+            icon: changePasswordFetching ? <LoadingOutlined /> : <EditOutlined />,
+            className: 'cvat-header-menu-change-password',
+            onClick: () => switchChangePasswordModalVisible(true),
+            label: t('Change password'),
+            disabled: changePasswordFetching,
+        }, 40]);
+    }
 
     menuItems.push([{
         key: 'logout',
         icon: logoutFetching ? <LoadingOutlined /> : <LogoutOutlined />,
         onClick: () => history.push('/auth/logout'),
-        label: 'Logout',
+        label: t('Logout'),
         disabled: logoutFetching,
     }, 50]);
 
@@ -415,7 +472,7 @@ function HeaderComponent(props: Props): JSX.Element {
                         history.push('/projects');
                     }}
                 >
-                    Projects
+                    {t('Projects')}
                 </Button>
                 <Button
                     className={getButtonClassName('tasks')}
@@ -427,7 +484,7 @@ function HeaderComponent(props: Props): JSX.Element {
                         history.push('/tasks');
                     }}
                 >
-                    Tasks
+                    {t('Tasks')}
                 </Button>
                 <Button
                     className={getButtonClassName('jobs')}
@@ -439,7 +496,7 @@ function HeaderComponent(props: Props): JSX.Element {
                         history.push('/jobs');
                     }}
                 >
-                    Jobs
+                    {t('Jobs')}
                 </Button>
                 <Button
                     className={getButtonClassName('cloudstorages')}
@@ -451,7 +508,7 @@ function HeaderComponent(props: Props): JSX.Element {
                         history.push('/cloudstorages');
                     }}
                 >
-                    Cloud Storages
+                    {t('Cloud storages')}
                 </Button>
                 <Button
                     className={getButtonClassName('requests')}
@@ -463,7 +520,7 @@ function HeaderComponent(props: Props): JSX.Element {
                         history.push('/requests');
                     }}
                 >
-                    Requests
+                    {t('Requests')}
                 </Button>
                 {isModelsPluginActive ? (
                     <Button
@@ -476,7 +533,7 @@ function HeaderComponent(props: Props): JSX.Element {
                             history.push('/models');
                         }}
                     >
-                        Models
+                        {t('Models')}
                     </Button>
                 ) : null}
                 {isAnalyticsPluginActive && user.hasAnalyticsAccess ? (
@@ -489,12 +546,12 @@ function HeaderComponent(props: Props): JSX.Element {
                             window.open('/analytics', '_blank');
                         }}
                     >
-                        Analytics
+                        {t('Analytics')}
                     </Button>
                 ) : null}
             </div>
             <div className='cvat-right-header'>
-                <CVATTooltip overlay='Click to open repository'>
+                <CVATTooltip overlay={`${tHeader('Click to open')}${t('repository')}`}>
                     <Button
                         icon={<GithubOutlined />}
                         size='large'
@@ -507,7 +564,7 @@ function HeaderComponent(props: Props): JSX.Element {
                         }}
                     />
                 </CVATTooltip>
-                <CVATTooltip overlay='Click to open guide'>
+                <CVATTooltip overlay={`${tHeader('Click to open')}${t('guide')}`}>
                     <Button
                         icon={<QuestionCircleOutlined />}
                         size='large'
